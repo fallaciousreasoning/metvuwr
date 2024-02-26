f1f1e46c8fb3b83a7363fc72a33b97b0529a6a88
Jay Harris <jay.harris@outlook.co.nz>
2024-02-26 16:25:34 +1300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+
+const App: Component = () => {
+  return (
+    <div class="bg-black w-5 h-5">
+    </div>
+  );
+};
+
+export default App;
