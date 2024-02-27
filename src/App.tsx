ac3a26eb57635c988df7a0ef84f72478713291f1
Jay Harris <jay.harris@outlook.co.nz>
2024-02-27 15:13:19 +1300

    Preload images

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import { createStore } from "solid-js/store";
 import Scrubber from './components/Scrubber';
 
 const [forecast, setForecast] = createStore<Forecast[]>([])
-toArray(getForecastImages()).then(setForecast)
+toArray(getForecastImages()).then(forecast => {
+  setForecast(forecast)
+
+  // preload images, to make navigating more seamless
+  for (const fc of forecast)
+    fetch(fc.url, { mode: 'no-cors' })
+})
 
 const App: Component = () => {
   return<Scrubber forecasts={forecast} />
